2a9583b4b5301165b2dbd5488ed922a2bd7e7395
Jan Holesovsky <kendy@suse.cz>
2009-03-10 11:05:59 +0100

    Now read the committers from the file.

--- committers.cxx
+++ committers.cxx
@@ -1,5 +1,6 @@
 #include "committers.hxx"
 
+#include <fstream>
 #include <string>
 
 using namespace std;
@@ -10,6 +11,31 @@ Committers::Committers()
 
 void Committers::load( const char *fname )
 {
+    ifstream input( fname, ifstream::in );
+    string line;
+
+    while ( !input.eof() )
+    {
+        getline( input, line );
+
+        // comments
+        if ( line.length() == 0 || line[0] == '#' )
+            continue;
+
+        // find the separators
+        size_t delim1 = line.find( "|" );
+        if ( delim1 == string::npos )
+            continue;
+        
+        size_t delim2 = line.find( "|", delim1 + 1 );
+        if ( delim2 == string::npos )
+            continue;
+
+        // store the data
+        const char* login = strdup( line.substr( 0, delim1 ).c_str() );
+        committers[login] = Committer( line.substr( delim1 + 1, delim2 - delim1 - 1 ),
+                                       line.substr( delim2 + 1 ) );
+    }
 }
 
 const Committer& Committers::getAuthor( const char* name )
@@ -18,9 +44,9 @@ const Committer& Committers::getAuthor( const char* name )
 
     if ( it == committers.end() )
     {
-        fprintf( stderr, "Author '%s' is missing, adding as '%s@openoffice.org'.", name, name );
+        fprintf( stderr, "ERROR: Author '%s' is missing, adding as '%s@openoffice.org'.", name, name );
 
-        return ( committers[ name ] = Committer( string( name ), string( name ) + "@openoffice.org" ) );
+        return ( committers[ strdup( name ) ] = Committer( string( name ), string( name ) + "@openoffice.org" ) );
     }
 
     return it->second;
