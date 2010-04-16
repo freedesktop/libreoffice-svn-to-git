2adbc91713f8c2186c6b55d3818f2cc70c9dbfa3
Jan Holesovsky <kendy@suse.cz>
2010-04-16 18:19:19 +0200

    Make the example in tag-hg-to-git.sh actually working.

--- tags-hg-to-git.sh
+++ tags-hg-to-git.sh
@@ -4,16 +4,17 @@
 # Just a quick hack ;-)
 #
 # Use like:
-# for I in * ; do ( cd $I ; echo -n ":tag map=$I," ; /local/projects/svn-to-git/tags-hg-to-git.sh ) ; done > /local/projects/svn-to-git/tags-hg-to-git.sh
+# cd ooo-build/src/clone
+# for I in * ; do [ -d $I ] && ( cd $I ; echo -n ":commit map=$I," ; /local/projects/svn-to-git/tags-hg-to-git.sh ) ; done > repositories.txt
 #
 
-( cd /local/home/ooogit/mercurial/DEV300 ; hg tags ) > /tmp/tags
+( cd /local/projects/mercurial/OOO320 ; hg tags ) > /tmp/tags
 
 for T in `git tag` 
 do
     N=`echo $T | sed 's#^ooo/##'`
     ( grep -m 1 "$N" /tmp/tags &&
-      git show $T | grep '^commit' | head -n 1 | sed 's/commit //' ) | tr ':\n' '  '
+      git show $T | grep -m 1 '^commit ' | sed 's/commit //' ) | tr ':\n' '  '
     echo
 done | while read TAG COMMIT HG GIT
 do
